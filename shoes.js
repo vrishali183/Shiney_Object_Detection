c2ff854e684ebc8d54f3771e34098c526168edbe
vrishali183 <vrishali183@gmail.com>
2022-04-08 16:38:19 +0530

    Add files via upload

---
+++ shoes.js
@@ -0,0 +1,47 @@
+shoes_img="";
+status="";
+objects=[];
+
+function preload(){
+    img=loadImage('Small room.jpg'); 
+}
+
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectdetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status :Detecting  objects";
+}
+
+function modelLoaded(){
+    console.log("model loaded");
+     status=true;
+     objectdetector.detect(img,gotResult);
+ }
+
+ function gotResult(error,Results){
+    if(error){
+    console.log(error);
+    } 
+  else{
+  console.log(Results);  
+  objects=Results;
+  
+  }
+}
+
+function draw(){
+    image(img,0,0,640,420); 
+    if(status!=""){
+        document.getElementById("status").innerHTML="Status:Object Detected"
+        fill('red')
+        for(i=0;i<objects.length;i++){
+                percent=floor(objects[i].confidence*100);
+                text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+                noFill();
+                stroke('red');
+
+                rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    } 
+}
